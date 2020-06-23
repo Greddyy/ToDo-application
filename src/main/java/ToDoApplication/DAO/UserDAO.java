01f2788965bf0da85e13e7b91effb85e0af5728c
Greddyy <55572482+Greddyy@users.noreply.github.com>
2020-06-23 15:53:09 +0300

    deleted loginUser method throws exception signatuer

--- src/main/java/ToDoApplication/DAO/UserDAO.java
+++ src/main/java/ToDoApplication/DAO/UserDAO.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class UserDAO {
 
 
-    public User loginUser(String username, String  password) throws SQLException {
+    public User loginUser(String username, String  password) {
 
         User user = null;
         ResultSet resultSet = null;
